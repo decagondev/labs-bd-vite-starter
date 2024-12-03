3f9eef790ebef72cf84763d7c981bfe3bd6f2396
Tom Tarpey <tomtarpeydev@gmail.com>
2024-12-03 18:32:56 +0000

    initial starter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import StarterCard from './components/StarterCard'
+
+function App() {
+
+  return (
+    <div className="flex mx-auto align-center justify-center gap-4 m-6">
+      <StarterCard />
+      <StarterCard />
+      <StarterCard />
+      <StarterCard />
+    </div>
+  )
+}
+
+export default App
